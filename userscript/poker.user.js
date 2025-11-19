dcc7b218476bf44f569645b6b74e8f9ff10801cb
Nicholas Doherty <nicholasdoherty95@gmail.com>
2025-11-19 16:32:55 -0500

    Add unsafeWindow

--- userscript/poker.user.js
+++ userscript/poker.user.js
@@ -3,7 +3,7 @@
 // @author       @UnbelievableBro
 // @namespace    http://tampermonkey.net/
 // @copyright    CC0
-// @version      1.1.2
+// @version      1.1.3
 // @description  https://www.tampermonkey.net/documentation.php
 // @icon         https://watchpeopledie.tv/icon.webp
 // @grant        unsafeWindow
@@ -13,50 +13,68 @@
 // @updateURL    https://raw.githubusercontent.com/adastra1826/wpd-poker/main/userscript/poker.user.js
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    function getGameState() {
-        return document.getElementById("poker-table").getAttribute("data-state");
-    }
+  function getGameState() {
+    return document.getElementById("poker-table").getAttribute("data-state");
+  }
 
-    function parseGameState() {
-        const gameState = getGameState();
-        const gameStateObj = JSON.parse(gameState);
-        console.log(gameStateObj);
-    }
+  function parseGameState() {
+    const gameState = getGameState();
+    const gameStateObj = JSON.parse(gameState);
+    console.log(gameStateObj);
+  }
 
-    const observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (mutation.target.id === "orgy-top-container") {
-                parseGameState();
-            }
-        });
+  const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+      if (mutation.target.id === "orgy-top-container") {
+        parseGameState();
+      }
     });
+  });
 
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-    });
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
 
-    function setInitialGameState() {
-        // Delete useless Wikipedia link
-        document.getElementById("poker-help-icon").remove();
-        console.log("Removed poker help icon/Wikipedia link");
+  function setInitialGameState() {
+    // Delete useless Wikipedia link
+    document.getElementById("poker-help-icon").remove();
+    console.log("Removed poker help icon/Wikipedia link");
 
-        // test
-        unsafeWindow._wpd_auto.check();
-        console.log("Checked");
-    }
+    // test
+    unsafeWindow._wpd_auto.check();
+    console.log("Checked");
+  }
 
-    // Runs automatically on page load
-    // Inject unsafeWindow script
-    (function() {
-        const s = document.createElement("script");
-        // v1
-        /*
-        s.textContent = `
-            window._wpd_auto = {
+  // Inject unsafeWindow script
+  window.addEventListener("DOMContentLoaded", () => {
+    const s = document.createElement("script");
+    // v1
+    /*
+    s.textContent = `
+        window._wpd_auto = {
+            test: () => console.log("Test OK"),
+            check: () => check(),
+            call: () => call(),
+            fold: () => fold(),
+            raiseTo: (amt) => {
+                document.querySelector("#raise-amount").value = amt;
+                raise();
+            },
+            start: () => startGame(),
+            ready: () => ready(true)
+        };
+
+        console.log("Injected non-sandboxed script for proxy function calling");
+    `;
+    */
+    // v2
+    s.textContent = `
+        Object.defineProperty(window, "_wpd_auto", {
+            value: {
                 test: () => console.log("Test OK"),
                 check: () => check(),
                 call: () => call(),
@@ -67,40 +85,17 @@
                 },
                 start: () => startGame(),
                 ready: () => ready(true)
-            };
+            },
+            writable: false,
+            configurable: false,
+            enumerable: false
+        });
 
-            console.log("Injected non-sandboxed script for proxy function calling");
-        `;
-        */
-        // v2
-        (function() {
-            const s = document.createElement("script");
-            s.textContent = `
-                Object.defineProperty(window, "_wpd_auto", {
-                    value: {
-                        test: () => console.log("Test OK"),
-                        check: () => check(),
-                        call: () => call(),
-                        fold: () => fold(),
-                        raiseTo: (amt) => {
-                            document.querySelector("#raise-amount").value = amt;
-                            raise();
-                        },
-                        start: () => startGame(),
-                        ready: () => ready(true)
-                    },
-                    writable: false,
-                    configurable: false,
-                    enumerable: false
-                });
+        console.log("Injected non-sandboxed script for proxy function calling");
+    `;
 
-                console.log("Injected non-sandboxed script for proxy function calling");
-            `;
-            document.documentElement.appendChild(s);
-        })();
-        
-        document.documentElement.appendChild(s);
-    })();
+    document.documentElement.appendChild(s);
+  })();
 
-    setInitialGameState();
+  setInitialGameState();
 })();
