23d2ade6078fbea25fe03077c916e590ffd251d4
Nicholas Doherty <nicholasdoherty95@gmail.com>
2025-11-19 15:32:02 -0500

    First pass with userscript. Lint on one of the raw=data files

--- userscript/poker.user.js
+++ userscript/poker.user.js
@@ -1,24 +1,45 @@
 // ==UserScript==
-// @name         New Userscript
-// @author       Nicholas Doherty
+// @name         WPD Poker Enhancement
+// @author       @UnbelievableBro
 // @namespace    http://tampermonkey.net/
 // @copyright    CC0
-// @version      2025-04-24
+// @version      1.0.0
 // @description  https://www.tampermonkey.net/documentation.php
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @icon         https://watchpeopledie.tv/icon.webp
 // @grant        none
 // @require
 // @include      *
-// @match        *
+// @match        https://watchpeopledie.tv/casino/poker/*
 // @exclude      *
 // @connect      *
 // @run-at       document-idle
-// @downloadURL  https://raw.githubusercontent.com/adastra1826/Scripts/refs/heads/main/Tampermonkey/0_TEMPLATE.user.js
-// @updateURL    https://raw.githubusercontent.com/adastra1826/Scripts/refs/heads/main/Tampermonkey/0_TEMPLATE.user.js
+// @downloadURL  https://raw.githubusercontent.com/adastra1826/wpd-poker/main/userscript/poker.user.js
+// @updateURL    https://raw.githubusercontent.com/adastra1826/wpd-poker/main/userscript/poker.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Your code here
+    function getGameState() {
+        return document.getElementById("poker-table").getAttribute("data-state");
+    }
+
+    function parseGameState() {
+        const gameState = getGameState();
+        const gameStateObj = JSON.parse(gameState);
+        console.log(gameState);
+    }
+
+    const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.target.id === "orgy-top-container") {
+                parseGameState();
+            }
+        });
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
 })();
